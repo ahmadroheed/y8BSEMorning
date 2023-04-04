a0c4ff31287ddd2e2ef3b2926f2cc7604c46f3a3
ahmadroheed <arkhaliqyar@gmail.com>
2023-04-04 10:14:24 +0430

    Activity code is added

---
+++ app/src/main/java/com/example/y8eveningapp/Second_Activity.java
@@ -0,0 +1,14 @@
+package com.example.y8eveningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Second_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
